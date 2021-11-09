65a03d35ef3dbf201325151ae2fa0966ee335bbc
Dibyendu Das <dibyendu.das.in@gmail.com>
2021-11-09 04:07:56 +0000

    updated URL

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 // export const WEB_SOCKET_URI = `wss://${import.meta.env.CLIENT_BASE_URL}${import.meta.env.CLIENT_WEBSOCKET_PATH}`
 // export const BASE_URI = import.meta.env.CLIENT_BASE_URL
 
-export const WEB_SOCKET_URI = 'wss://Ultimate-Tic-Tac-Toe.dibyendudas1.repl.co/ping'
+export const WEB_SOCKET_URI = 'wss://ultimate-tic-tac-toe.dibyendu-das.repl.co/ping'
 
-export const BASE_URI = 'https://Ultimate-Tic-Tac-Toe.dibyendudas1.repl.co'
+export const BASE_URI = 'https://ultimate-tic-tac-toe.dibyendu-das.repl.co'
